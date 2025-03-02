53a665a3e1e2ea4efd2909d850819166d554a8a6
MKing <wlx@leixin.wang>
2025-03-03 00:56:15 +0800

    Update version number to 1.33.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ elif (3, 0) < version < (3, 5):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '1.32'
+VERSION = '1.33'
 
 install_requires = ['psutil', 'colorama', 'six','langchain-core','langchain','langchain-openai','langchain-anthropic','langchain-deepseek']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
